434c2c3891d797c8a1ce1f3b41858fdd0f6b0ee6
AD-7 <dominiak.aleksander@onet.pl>
2018-11-05 15:07:59 +0100

    Reflector + assemblymetadata model

---
+++ Data/Metadata_Model/AssemblyMetadata.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Metadata_Model
+{
+    public class AssemblyMetadata
+    {
+        public string Name { get; set; }
+
+
+
+
+
+
+        internal AssemblyMetadata(Assembly assembly) 
+        {
+            Name = assembly.ManifestModule.Name;
+
+        }
+    }
+}
---
+++ Data/Reflector.cs
@@ -0,0 +1,28 @@
+﻿using Data.Metadata_Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class Reflector
+    {
+        
+        public AssemblyMetadata AssemblyModel { get; private set; }
+
+
+        public void Reflect(string path)
+        {
+            Assembly assembly = Assembly.LoadFrom(path);
+            AssemblyModel = new AssemblyMetadata(assembly);    
+        }
+
+
+
+
+
+    }
+}
