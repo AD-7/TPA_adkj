c90b77b51823735d36d1e6bfb7b4cee28584af27
AD-7 <dominiak.aleksander@onet.pl>
2018-11-07 15:27:06 +0100

    Trace ready

--- Data/Tracing/TraceSource.cs
+++ Data/Tracing/TraceSource.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,9 +11,23 @@ namespace Data.Tracing
 
     public class TraceSource : ITraceSource
     {
+        string file;
+        public TraceSource(string file)
+        {
+            this.file = file;
+        }
         public void TraceData(TraceEventType eventType, object data)
         {
-            throw new NotImplementedException();
+            DateTime now = DateTime.Now;
+            string message;
+            message = eventType.ToString() + "->";
+            message += "(" + now.ToString() + ")";
+            message += data.ToString() + "/n/n";
+            byte[] tmpMessage = new UTF8Encoding(true).GetBytes(message); 
+            using (FileStream fs = new FileStream(file, FileMode.Append, FileAccess.Write, FileShare.Read))
+            {
+                fs.Write(tmpMessage, 0, tmpMessage.Length);
+            }
         }
     }
 }
