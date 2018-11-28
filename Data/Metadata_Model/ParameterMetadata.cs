da47b0d5d6c7a2fb09ddccad08e739e782c9aafc
AD-7 <dominiak.aleksander@onet.pl>
2018-11-28 14:30:08 +0100

    Create ParameterMetadata.cs

---
+++ Data/Metadata_Model/ParameterMetadata.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Data.Metadata_Model
+{
+    public class ParameterMetadata : IMetadata
+
+    {
+        public string Name { get; private set; }
+        public string MetadataName { get; set; }
+        public TypeMetadata Type;
+
+        public ParameterMetadata(string name, TypeMetadata type)
+        {
+            Name = name;
+            MetadataName = "Parameter: ";
+            Type = type;
+        }
+
+        public ObservableCollection<IMetadata> getChildren
+        {
+
+            get
+            {
+                ObservableCollection<IMetadata> children = new ObservableCollection<IMetadata>();
+                children.Add(Type);
+                return children;
+            }
+        }
+    }
+}
