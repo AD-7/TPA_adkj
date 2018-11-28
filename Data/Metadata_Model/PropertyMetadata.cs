b76f6191fe933073519752c10a35201193cdcfd7
AD-7 <dominiak.aleksander@onet.pl>
2018-11-28 14:32:20 +0100

    chyba będzie juz ok

---
+++ Data/Metadata_Model/PropertyMetadata.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Data.Metadata_Model
+{
+    public class PropertyMetadata : IMetadata
+    {
+        public string Name { get; private set; }
+        public string MetadataName { get; set; }
+        private TypeMetadata Type;
+
+        public PropertyMetadata(string Name, TypeMetadata type)
+        {
+            this.Name = Name;
+            MetadataName = "Property: ";
+            Type = type;
+        }
+
+
+
+        public ObservableCollection<IMetadata> getChildren
+        {
+
+            get
+            {
+                ObservableCollection<IMetadata> children = new ObservableCollection<IMetadata>();
+
+                children.Add(Type);
+                return children;
+            }
+        }
+    }
+}
