624b50c4c020f58c99b21dc1a704ca4f8cbfd249
AD-7 <dominiak.aleksander@onet.pl>
2019-01-17 20:26:00 +0100

    Update ReflectionTests.cs

--- ReflectionTests/ReflectionTests.cs
+++ ReflectionTests/ReflectionTests.cs
@@ -1,14 +1,56 @@
-﻿using System;
+﻿using DTG;
+using DTG_Transfer.Service;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Reflection.MapperToDTG;
+using Reflection.Metadata_Model;
+using Serialization;
+using System.IO;
 
 namespace ReflectionTests
 {
     [TestClass]
-    public class ReflectionTests
+    public class SerializationTest
     {
-        [TestMethod]
-        public void TestMethod1()
+        ISerialization ser;
+        Reflector refl;
+        AssemblyDTG dtg;
+
+        [TestInitialize]
+        public void Init()
         {
+            ser = new SerXML();
+            refl = new Reflector();
+            refl.Reflect("Reflection.dll");
+            dtg = MapperToDTG.AssemblyDtg(refl.AssemblyModel);
+
         }
+
+        [TestMethod]
+        public void SerTest1()
+        {
+
+
+            ser.Serialize(dtg);
+            Assert.IsTrue(File.Exists("fileSave.xml") == true);
+        }
+
+
+        [TestMethod]
+        public void SerTest2()
+        {
+            ser.Serialize(dtg);
+            AssemblyDTG tmp = ser.Deserialize();
+
+            Assert.AreEqual(tmp.Name, "Reflection.dll");
+        }
+
+        [TestMethod]
+        public void SerTest3()
+        {
+            ser.Serialize(dtg);
+            AssemblyDTG tmp = ser.Deserialize();
+            Assert.AreEqual(tmp.Namespaces.Count, 3);
+        }
+
     }
 }
