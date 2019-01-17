9a2c87a74ddd010e8e59ed177a8d4cb7622496a2
KamilJakubik <44211205+KamilJakubik@users.noreply.github.com>
2019-01-17 23:07:58 +0100

    testy serializacja baza

--- DbSerializationTests/DBSerTest.cs
+++ DbSerializationTests/DBSerTest.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿﻿using System;
+using System.IO;
 using DatabaseSerialization;
 using DTG;
 using DTG_Transfer.Service;
@@ -28,6 +29,8 @@ namespace DbSerializationTests
         [TestMethod]
         public void TestMethod1()
         {
+            ser.Serialize(dtg);
+            Assert.IsTrue(File.Exists("fileSave.xml") == true);
         }
     }
 }
