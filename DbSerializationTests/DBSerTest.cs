9f33dc7403f3a2f6496db4e94dbd84b50f1678b5
KamilJakubik <44211205+KamilJakubik@users.noreply.github.com>
2019-01-17 23:41:34 +0100

    Update DBSerTest.cs

--- DbSerializationTests/DBSerTest.cs
+++ DbSerializationTests/DBSerTest.cs
@@ -27,10 +27,53 @@ namespace DbSerializationTests
 
 
         [TestMethod]
-        public void TestMethod1()
+        public void CheckAssemblyNameAfterDeserialization()
         {
             ser.Serialize(dtg);
-            Assert.IsTrue(File.Exists("fileSave.xml") == true);
+            AssemblyDTG tmp = ser.Deserialize();
+            Assert.AreEqual(tmp.Name, "Reflection.dll");
         }
+
+        [TestMethod]
+        public void CheckNumberOfNamespaces()
+        {
+            ser.Serialize(dtg);
+            AssemblyDTG tmp = ser.Deserialize();
+            Assert.AreEqual(tmp.Namespaces.Count, 3);
+        }
+
+        [TestMethod]
+        public void CheckNamespaceName()
+        {
+            ser.Serialize(dtg);
+            AssemblyDTG tmp = ser.Deserialize();
+
+            Assert.AreEqual("Reflection.SaveManager", tmp.Namespaces[0].Name);
+            Assert.AreEqual("Reflection.Metadata_Model", tmp.Namespaces[1].Name);
+            Assert.AreEqual("Reflection.MapperToDTG", tmp.Namespaces[2].Name);
+        }
+
+        [TestMethod]
+        public void CheckFirstMethodName()
+        {
+            ser.Serialize(dtg);
+            AssemblyDTG tmp = ser.Deserialize();
+
+            Assert.AreEqual("get_Name", tmp.Namespaces[0].Types[0].SerMethods[0].Name);
+        }
+
+
+        [TestMethod]
+        public void CheckFirstPropertieName()
+        {
+            ser.Serialize(dtg);
+            AssemblyDTG tmp = ser.Deserialize();
+
+            Assert.AreEqual("Name", tmp.Namespaces[0].Types[0].SerProperties[0].Name);
+        }
+
+
+
+
     }
 }
