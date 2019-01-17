6394aaf0276247683928eea3cc395f0afc4354b5
AD-7 <dominiak.aleksander@onet.pl>
2019-01-17 20:43:46 +0100

    Dodaje projekty dla testów dwóch serializacji
    Dodać jeszcze trzeba jakieś testy w tych projektach

---
+++ DbSerializationTests/DBSerTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using DatabaseSerialization;
+using DTG;
+using DTG_Transfer.Service;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Reflection.MapperToDTG;
+using Reflection.Metadata_Model;
+
+namespace DbSerializationTests
+{
+    [TestClass]
+    public class DBSerTest
+    {
+        ISerialization ser;
+        Reflector refl;
+        AssemblyDTG dtg;
+        [TestInitialize]
+        public void Init()
+        {
+            ser = new DbSerialization();
+            refl = new Reflector();
+            refl.Reflect("Reflection.dll");
+            dtg = MapperToDTG.AssemblyDtg(refl.AssemblyModel);
+
+        }
+
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
