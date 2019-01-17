85d8878d421e70b98fb98019e75edebeb33b8845
AD-7 <dominiak.aleksander@onet.pl>
2019-01-17 20:15:18 +0100

    ..

---
+++ UnitTestsData/SerializationTest.cs
@@ -0,0 +1,42 @@
+﻿using DTG;
+using DTG_Transfer.Service;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Reflection.MapperToDTG;
+using Reflection.Metadata_Model;
+using Serialization;
+using System.IO;
+
+namespace UnitTestsData
+{
+    [TestClass]
+    public class SerializationTest
+    {
+        ISerialization ser;
+        Reflector refl;
+
+        [TestInitialize]
+        public void Init()
+        {
+            ser = new SerXML();
+            refl = new Reflector();
+            refl.Reflect("Reflection.dll");
+            
+        }
+
+        [TestMethod]
+        public void SerTest1()
+        {
+            AssemblyDTG dtg = MapperToDTG.AssemblyDtg(refl.AssemblyModel);
+
+            ser.Serialize(dtg, "path");
+            Assert.IsTrue(File.Exists("path.xml") == true);        
+        }
+
+
+
+
+
+
+
+    }
+}
